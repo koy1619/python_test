683822f7b141eb860140fd4ce659c51ae76799fc
koy1619 <maxiaolei007@sina.com>
2014-05-24 20:36:32 +0800

    update

---
+++ class_object/leve.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#coding=utf8
+'''
+#=============================================================================
+#     FileName: leve.py
+#         Desc: <F4>
+#       Author: xiaolei ma
+#        Email: koy1619@gmail.com
+#     HomePage: http://www.linux48.com
+#      Version: 0.0.1
+#   LastChange: 2014-05-24 20:35:40
+#      History:
+#=============================================================================
+'''
+class superList(list):
+    def __sub__(self, b):
+        a = self[:]     # 这里，self是supeList的对象。由于superList继承于list，它可以利用和list[:]相同的引用方法来表示整个对象。
+        b = b[:]        
+        while len(b) > 0:
+            element_b = b.pop()
+            if element_b in a:
+                a.remove(element_b)
+        return a
+
+print superList([1,2,3]) - superList([3,4])
